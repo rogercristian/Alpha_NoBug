1f20bbeeae6332e72d90588d9c066c6eb6145847
Roger Cristian <erogennin@gmail.com>
2024-04-21 16:04:16 -0300

    Inimigos agora atiram

---
+++ Assets/_GAME/_Script/NPCs/Enemies/EnemyUseWeapon.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyUseWeapon : MonoBehaviour
+{
+    [SerializeField] Weapon weapon;
+    NavMeshAgent agent;
+    Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon = GetComponent<Weapon>();
+        agent = GetComponentInParent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (agent != null)
+        {
+            player = FindAnyObjectByType<MovingPlayer>().transform;
+            float dist = Vector3.Distance(transform.position, player.transform.position);
+
+            if (dist <= agent.stoppingDistance)
+            {
+                weapon.Shoot();
+            }
+        }
+    }
+}
--- Assets/_GAME/_Script/Shared/NavigationAgentControl.cs
+++ Assets/_GAME/_Script/Shared/NavigationAgentControl.cs
@@ -5,12 +5,16 @@ public class NavigationAgentControl : MonoBehaviour
 {
     NavMeshAgent agent;
     [SerializeField] MovingPlayer[] target;
+    [SerializeField] float repeatRate = 5f;
+    [SerializeField] float time = .1f;
+    Weapon weapon;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        weapon = GetComponentInChildren<Weapon>();
         agent.autoBraking = false;
-        InvokeRepeating("Reapete", .5f, 5f);
+        InvokeRepeating("Reapete", time, repeatRate);
     }
     void Reapete()
     {
@@ -20,6 +24,7 @@ public class NavigationAgentControl : MonoBehaviour
 
         int rand = Random.Range(0, target.Length);
         agent.destination = target[rand].transform.position;
-        Debug.Log(rand);
+     
+        //Debug.Log(rand);
     }
 }
